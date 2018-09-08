d29ddff357e3f5d4d704887e8fda482b9960e1cf
GarethPGH <garethlwarren@gmail.com>
2018-09-07 21:08:26 -0400

    Reorganized project
    Added folders for files
    
    Included my own artwork for the site images
    
    Preparing to add information dynamically to the HTML through Javascript and .json files to remove excess "space" caused by hidden information divs.

--- RedWolves/DropdownController.js
+++
@@ -1,7 +0,0 @@
-﻿//document.addEventListener("DOMContentLoaded", function (event) {
-//    var References = document.getElementById("Dropdown");
-//    References.addEventListener("onmouseover", function (References){
-//        References = this;
-//        this.style.visibility = "visible";
-//    });
-//});
---
+++ RedWolves/Scripts/DropdownController.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener("DOMContentLoaded", function (event) {
+    Dropdowns.init();
+    var foot = document.getElementById("Foot");
+    var menu = document.getElementById("Dropdown");
+
+    foot.addEventListener('onmouseover', function () {
+        Dropdowns.render('menu', { isOpen: 'true' });
+    });
+    
+});
