c613db2335399f901b85bbbe970e14210803faf9
GarethPGH <garethlwarren@gmail.com>
2018-09-14 21:10:37 -0400

    Only In Programming
    Will you find a more complicated way of writing the same exact thing.
    *Headdesk*
    
    Well the Content shows up, in the same place it did with the LAST iteration.

--- RedWolves/Scripts/ContentButtonController.js
+++ RedWolves/Scripts/ContentButtonController.js
@@ -8,68 +8,62 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var RedWolfButton = document.getElementById("RedWolf");
     var CoyoteButton = document.getElementById("Coyote");
 
-    function HideContent() {
-
-        var pages = document.getElementsByClassName('Content');
-
-        for (var i = 0; i < pages.length; i++) {
-            if (pages[i].style.display === 'flex') {
-                pages[i].classList.add('hide');
-            }
-        }
-    }
-
-    function ShowContent(Info) {
-        console.log(Info);
-        Info.style.display = 'flex';
-        //toggles class list, but the added class don't really DO anything they are supposed to.
-        if (Info.classList.contains('hide')) {
-            Info.classList.remove('hide');
-            Info.classList.add('show');
-        } else {
-            Info.classList.remove('show');
-            Info.classList.add('hide');
-        }
-        //var hasChildren = Info.hasChildNodes;
-
-        //if (hasChildren) {
-        //    Info.childNodes.style.display = 'flex';
-        //    Info.childNodes.style.visibility = 'visible';
-        //}
-    }
-
     function HideLanding() {
         var LandingImage = document.getElementById("Landing");
         LandingImage.style.display = 'none';
     }
-    
-    WolfButton.addEventListener("click", function (event) {
 
-       // HideContent();
+    function HideAllContent() {
+        var Info = document.getElementsByClassName('Content');
+        for (let i = 0; i < Info.length; i++) {
+            Info[i].classList.add('hide');
+        }
+    }
+
+    function ShowHideContent(Cont) {
+
+        console.log(Cont);
+
+        Cont.style.visibility = 'visible';
+
+        if (Cont.className === 'hide') {
+            Cont.className = 'show';
+        } else {
+            Cont.className = 'hide';
+        }
+    }
+
+    WolfButton.addEventListener("click", function (event) {
+        
         HideLanding();
 
+        HideAllContent();
+
         var element = document.getElementById('WolfContent');
-        ShowContent(element);
-        
+      
+        ShowHideContent(element);
     });
 
     RedWolfButton.addEventListener("click", function (event) {
 
-       // HideContent();
         HideLanding();
 
+        HideAllContent();
+
         var element = document.getElementById('RedWolfContent');
-        ShowContent(element);
       
+        ShowHideContent(element);
     });
 
     CoyoteButton.addEventListener("click", function (event) {
 
-       // HideContent();
         HideLanding();
 
+        HideAllContent();
+
         var element = document.getElementById('CoyoteContent');
-        ShowContent(element);
+
+        ShowHideContent(element);
        
     }); 
  
