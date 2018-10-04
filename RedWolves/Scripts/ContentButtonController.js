914587bb4303b578b1e458bb128e720047cf99db
GarethPGH <garethlwarren@gmail.com>
2018-10-03 21:12:30 -0400

    Feel Like I am Getting Somewhere
    Still not entirely sure, but at least HideLanding does what it is supposed to, though, unfortunately, on its own without a button click

--- RedWolves/Scripts/ContentButtonController.js
+++ RedWolves/Scripts/ContentButtonController.js
@@ -2,43 +2,66 @@
 //Hides and Shows content based on button press.
 
 document.addEventListener("DOMContentLoaded", function (event) {
-    //Behaves as expected, however, does not show child content.
 
-    var WolfButton = document.getElementById("Wolf");
-    var RedWolfButton = document.getElementById("RedWolf");
-    var CoyoteButton = document.getElementById("Coyote");
-
-    
-
+    //Works automatically now....
     var HideLanding = function () {
         var LandingImage = document.getElementById("Landing");
         LandingImage.style.display = 'none';
     };
 
-    //    var Info = document.getElementsByClassName('Content');
-    //    for (let i = 0; i < Info.length; i++) {
-    //        Info[i].classList.add('hide');
-    //    }
+        /*Still does not work, but shows this in the console
+         * <button id="Wolf" class="Btn">
+    ContentButtonController.js: 91: 9
+    Wolf ContentButtonController.js: 98: 9
+    [object HTMLDivElement][object HTMLDivElement][object HTMLDivElement]ContentButtonController.js: 117: 9
+    classes: Content hide display: ContentButtonController.js: 134: 9
+        < button id = "RedWolf" class="Btn" >
+            ContentButtonController.js: 91: 9
+    RedWolf ContentButtonController.js: 98: 9
+    [object HTMLDivElement][object HTMLDivElement][object HTMLDivElement]ContentButtonController.js: 117: 9
+    classes: Content hide display: none ContentButtonController.js: 134: 9
+        < button id = "Coyote" class="Btn" >
+            ContentButtonController.js: 91: 9
+    Coyote ContentButtonController.js: 98: 9
+    [object HTMLDivElement][object HTMLDivElement][object HTMLDivElement]ContentButtonController.js: 117: 9
+    classes: Content hide display: none*/
+  
 
-    //    console.log(Cont);
+    var buttons = document.getElementsByClassName("Btn");
 
-    //    Cont.style.visibility = 'visible';
-    
-    //    if (Cont.className === 'hide') {
-    //        Cont.className.remove('hide');
-    //    } else {
-    //        Cont.className.add('hide');
-    //    }
-    //};
+    for (let i = 0; i < buttons.length; i++) {
+        console.log(buttons[i]);
+        var clicked = buttons[i].id;
+        buttons[i].addEventListener('click', ShowHideElements(clicked));
+    }
 
+    function ShowHideElements(button) {
+        var element = button;
+        console.log(button);//why are you NULL!
+        var element1tohide;
+        var element2tohide;
 
-    WolfButton.addEventListener("click", function (event) {
+        if (button === 'Wolf') {
+            element = document.getElementById('WolfContent');
+            element1tohide = document.getElementById('RedWolfContent');
+            element2tohide = document.getElementById('CoyoteContent');
+        } else if (button === 'RedWolf') {
+            element = document.getElementById('RedWolfContent');
+            element1tohide = document.getElementById('WolfContent');
+            element2tohide = document.getElementById('CoyoteContent');
+        } else if (button === 'Coyote') {
+            element = document.getElementById('CoyoteContent');
+            element1tohide = document.getElementById('RedWolfContent');
+            element2tohide = document.getElementById('WolfContent');
+        }
+
+        //why is this null, undefined, undefined?
+        console.log(element + " " + element1tohide + " " + element2tohide);
+
+        //at least this works....automatically now... 
         HideLanding();
 
-        var element = document.getElementById('WolfContent');
-        var element1tohide = document.getElementById('RedWolfContent');
-        var element2tohide = document.getElementById('CoyoteContent');
-
+        //Undefined, therefore does not work
         element1tohide.style.display = 'none';
         element2tohide.style.display = 'none';
 
@@ -51,48 +74,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
             element.style.visibility = 'hidden';
         }
         console.log("classes: " + element.classList + " " + "display: " + element.style.display);
-    });
-
-    RedWolfButton.addEventListener("click", function (event) {
-        HideLanding();
-
-        var element = document.getElementById('RedWolfContent');
-        var element1tohide = document.getElementById('WolfContent');
-        var element2tohide = document.getElementById('CoyoteContent');
-
-        element1tohide.style.display = 'none';
-        element2tohide.style.display = 'none';
-
-        if (element.classList.contains('hide')) {
-            element.classList.remove('hide');
-            element.style.visibility = 'visible';
-            element.style.display = 'flex';
-        } else {
-            element.classList.add('hide');
-            element.style.visibility = 'hidden';
-        }
-        console.log("classes: " + element.classList + " " + "display: " + element.style.display);
-    });
-
-    CoyoteButton.addEventListener("click", function (event) {
-        HideLanding();
-
-        var element = document.getElementById('CoyoteContent');
-        var element1tohide = document.getElementById('RedWolfContent');
-        var element2tohide = document.getElementById('WolfContent');
-
-        element1tohide.style.display = 'none';
-        element2tohide.style.display = 'none';
-
-        if (element.classList.contains('hide')) {
-            element.classList.remove('hide');
-            element.style.visibility = 'visible';
-            element.style.display = 'flex';
-        } else {
-            element.classList.add('hide');
-            element.style.visibility = 'hidden';
-        }
-        console.log("classes: " + element.classList + " " + "display: " + element.style.display);
-    }); 
+    }
  
 });
