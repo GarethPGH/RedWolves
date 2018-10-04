63bb7dbfebf6e8523ebabe3e9e0aee3c27e12729
GarethPGH <garethlwarren@gmail.com>
2018-10-04 19:12:49 -0400

    Broke Some Things, Fixed Some Things, Broke Some More Things
    Figured out that my buttons were not recieving the right Id in the eventlistener.
    
    Figured out that HideLanding only works as a function when defined above the button event assignment.
    
    ShowHideElements still does not work. May have to do where it is also defined.
    
    May have to revisit Javascript's function hoisting.

--- RedWolves/Scripts/ContentButtonController.js
+++ RedWolves/Scripts/ContentButtonController.js
@@ -3,65 +3,49 @@
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    //Works automatically now....
+    //If defined BELOW button click assignment, recieve HideLanding is not a function
+    //Works automatically ABOVE button click assignment.
     var HideLanding = function () {
         var LandingImage = document.getElementById("Landing");
         LandingImage.style.display = 'none';
     };
 
-        /*Still does not work, but shows this in the console
-         * <button id="Wolf" class="Btn">
-    ContentButtonController.js: 91: 9
-    Wolf ContentButtonController.js: 98: 9
-    [object HTMLDivElement][object HTMLDivElement][object HTMLDivElement]ContentButtonController.js: 117: 9
-    classes: Content hide display: ContentButtonController.js: 134: 9
-        < button id = "RedWolf" class="Btn" >
-            ContentButtonController.js: 91: 9
-    RedWolf ContentButtonController.js: 98: 9
-    [object HTMLDivElement][object HTMLDivElement][object HTMLDivElement]ContentButtonController.js: 117: 9
-    classes: Content hide display: none ContentButtonController.js: 134: 9
-        < button id = "Coyote" class="Btn" >
-            ContentButtonController.js: 91: 9
-    Coyote ContentButtonController.js: 98: 9
-    [object HTMLDivElement][object HTMLDivElement][object HTMLDivElement]ContentButtonController.js: 117: 9
-    classes: Content hide display: none*/
-  
-
     var buttons = document.getElementsByClassName("Btn");
+    var clicked = new Array();
 
     for (let i = 0; i < buttons.length; i++) {
-        console.log(buttons[i]);
-        var clicked = buttons[i].id;
-        buttons[i].addEventListener('click', ShowHideElements(clicked));
+        clicked[i] = buttons[i].id;     
+        buttons[i].addEventListener('click', ShowHideElements(clicked[i]));
     }
 
-    function ShowHideElements(button) {
-        var element = button;
-        console.log(button);//why are you NULL!
+    ///This part still does not function
+    function ShowHideElements(buttonid) {
+        var buttonIdString = buttonid + "";
+        console.log(buttonIdString);
+        var element;
         var element1tohide;
         var element2tohide;
 
-        if (button === 'Wolf') {
+        if (buttonIdString === 'Wolf') {
             element = document.getElementById('WolfContent');
             element1tohide = document.getElementById('RedWolfContent');
             element2tohide = document.getElementById('CoyoteContent');
-        } else if (button === 'RedWolf') {
+        } else if (buttonIdString === 'RedWolf') {
             element = document.getElementById('RedWolfContent');
             element1tohide = document.getElementById('WolfContent');
             element2tohide = document.getElementById('CoyoteContent');
-        } else if (button === 'Coyote') {
+        } else if (buttonIdString === 'Coyote') {
             element = document.getElementById('CoyoteContent');
             element1tohide = document.getElementById('RedWolfContent');
             element2tohide = document.getElementById('WolfContent');
         }
 
-        //why is this null, undefined, undefined?
+       
         console.log(element + " " + element1tohide + " " + element2tohide);
 
-        //at least this works....automatically now... 
+        //at least this works
         HideLanding();
 
-        //Undefined, therefore does not work
         element1tohide.style.display = 'none';
         element2tohide.style.display = 'none';
 
