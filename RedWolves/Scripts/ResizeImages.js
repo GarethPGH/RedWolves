90c31e8572a8fba9445aaa2dbf89097c42fa6bc4
GarethPGH <garethlwarren@gmail.com>
2018-11-09 10:55:18 -0500

    Plethora of Changes
    Added About.html information
    
    Changed Project layout
    
    Changed filepath to new layout so CSS and JS work properly
    
    Switched off debug mode to go back to using Chrome Dev tools which proved easier
    
    Created a JS file to pick appropriate image size for the images.

---
+++ RedWolves/Scripts/ResizeImages.js
@@ -0,0 +1,26 @@
+﻿document.addEventListener('DOMContentLoaded', function (event) {
+
+    var ViewWidth = window.ViewWidth;
+    var WImg = document.getElementById("WolfImg");
+    var RImg = document.getElementById("RedWolfImg");
+    var CImg = document.getElementById("CoyoteImg");
+
+    console.log(ViewWidth);
+
+    if (ViewWidth >= 1000) {
+            RImg.src = //"Placeholder"
+            WImg.src = //Placeholder
+            CImg.src = //Placeholder
+    }else if (ViewWidth < 1000 && ViewWidth > 400) {
+            RImg.src = //"Placeholder"
+            WImg.src = //Placeholder
+            CImg.src = //Placeholder
+    }else if (ViewWidth <= 400) {
+            RImg.src = //"Placeholder"
+            WImg.src = //Placeholder
+            CImg.src = //Placeholder
+    }else {
+        console.log('Does not apply');
+    }
+    
+});
